e213d5b8f212bdb8fc8a1b824aafcb0e757b4c62
diegoala2004 <diegoala012004@gmail.com>
2026-04-30 14:34:40 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ const minioClient = new Minio.Client({
 const https = require('https');
 minioClient.transport = https;
 
-// Forzamos el puerto manualmente en el objeto para saltar restricciones
-minioClient.port = 443;
-
 app.post('/get-url', async (req, res) => {
   if (!req.body.input || !req.body.input.file_name) {
     return res.status(400).json({ error: "file_name is required" });
